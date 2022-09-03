917e39fc44d4e3776edc283007d2975f6f2f148f
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-09-03 16:48:25 +0200

    local storage with save button + cleanup

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,11 @@
+use std::time::Duration;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct TemplateApp {
     label: String,
 
-    #[serde(skip)]
     value: f32,
 }
 
@@ -28,19 +29,26 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
+    fn auto_save_interval(&self) -> Duration {
+        Duration::from_secs(60)
+    }
+
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         let Self { label, value } = self;
 
-        #[cfg(not(target_arch = "wasm32"))]
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
+                    #[cfg(not(target_arch = "wasm32"))]
                     if ui.button("Quit").clicked() {
-                        _frame.close();
+                        frame.close();
+                    }
+                    if ui.button("Save").clicked() {
+                        self.save(frame.storage_mut().unwrap());
                     }
                 });
             });
@@ -49,27 +57,11 @@ impl eframe::App for TemplateApp {
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Side Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
+                    egui::warn_if_debug_build(ui);
                 });
             });
         });
@@ -81,13 +73,6 @@ impl eframe::App for TemplateApp {
                 "https://github.com/emilk/eframe_template/blob/master/",
                 "Source code."
             ));
-            egui::warn_if_debug_build(ui);
         });
-
-        if true {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-            });
-        }
     }
 }
